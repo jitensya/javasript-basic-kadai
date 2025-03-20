b52cb4c03ed6e07ea7e5606d8c6c17f17edd5359
jitensya <knjmtm71+git@gmail.com>
2025-03-20 18:19:01 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -2,6 +2,7 @@ const usertaijyu = 68;
 const usershincyou1 = 1.7;
 let usershincyou;
 let userbmi;
-usershincyou = usershincyou1*2;
+usershincyou = usershincyou1*usershincyou1;
+;
 userbmi= usertaijyu/usershincyou;
 console.log(Math.floor(userbmi));
