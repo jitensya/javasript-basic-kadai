30b84a9ed77fe89640228d7350fc028cfe847e20
jitensya <knjmtm71+git@gmail.com>
2025-03-22 17:16:22 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,16 +1,15 @@
 let num = Boolean;
-let num3 = Boolean;
-let num5= Boolean;
+// let num3 = Boolean;
+// let num5= Boolean;
 num = Math.floor(Math.random()*50+1);
 
 
-num3 = num%3;
-num5 = num%5;
+let num3 = num%3;
+let num5 = num%5;
 
 if (num3==0 && num5==0)
 {
 console.log('3と5の倍数です')
-console.log(num);
 }
 
  else if ( num3 == 0){
