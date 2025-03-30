eab8f28dd812e7f015fcd5d8bddc7f5442afd3d3
jitensya <knjmtm71+git@gmail.com>
2025-03-30 16:06:10 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,14 @@
+// *const Btn = document.getElementById("btn");
+
+// Btn.addEventstener("click",() =>{
+// const bt2 = document.createElement();
+// textContent = "ボタンをクリックしました";
+// });
 const Btn = document.getElementById("btn");
+const comment = "ボタンをクリックしました";
 
-Btn.addEventListener("click",() =>{
-Btn.innerHTML = ("ボタンをクリックしました");
+Btn.addEventstener("click",() =>{
+    textContent. "comment";
 });
-
 
 
